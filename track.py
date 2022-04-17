19cdc35163cf985c338c8451b1b0f936c5a800af
Rishi Bhuptani <65431923+rishi-bhuptani@users.noreply.github.com>
2022-04-17 14:55:22 -0700

    Update track.py

--- track.py
+++ track.py
@@ -202,7 +202,7 @@ def detect(opt):
                             bbox_h = output[3] - output[1]
                             # Write MOT compliant results to file
                             with open(txt_path + '.txt', 'a') as f:
-                                f.write(('%g ' * 8 + '\n') % (frame_idx + 1, id, bbox_left,  
+                                f.write(('%g ' * 7 +'%s '+ '\n') % (frame_idx + 1, id, bbox_left,  
                                                                bbox_top, bbox_w, bbox_h, cls, names[c])
 
                         if save_vid or save_crop or show_vid:  # Add bbox to image
